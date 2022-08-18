847be44c2bce9be0215d50aa0b4b274f5ead610d
lcxz8686 <1585342785@qq.com>
2022-08-18 22:02:36 +0800

    商户查询缓存更新策略

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,67 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    // 基于Redis设置的拦截器
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        //判断是否要拦截
+        if (UserHolder.getUser() == null) {
+            response.setStatus(401);
+            return false;
+        }
+
+        //有用户，放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+
+    /*
+    // 基于Session的拦截器
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1. 获取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3. 判断用户是否存在
+        if (user == null){
+            //4. 不存在，拦截
+            response.setStatus(401);
+            return false;
+        }
+
+        //5. 存在 保存用户信息到ThreadLocal
+        UserHolder.saveUser((UserDTO) user);
+        //6. 放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+     */
+}
